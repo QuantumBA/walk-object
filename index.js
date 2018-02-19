8a225408e186898967d9a4cfc514b2a85ac07f51
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-02-19 20:22:56 +0100

    Refactor

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-module.exports = {
-  walkObject: require('./walk-object'),
-  walkObjectAsync: require('./walk-object-async')
+/**
+* @param {object} root - The object to walk
+* @param {function} fn - A function to call on each node
+*/
+module.exports = function walkObject(root, fn)
+{
+  async function walk(value, location = [])
+  {
+    // Value is an array, call walk on each item in the array
+    if(Array.isArray(value))
+      return await Promise.all(
+        fn({value, location}),
+        ...value.map((el, j) => walk(el, [...location, j]))
+      )
+
+    // Value is an object, walk the keys of the object
+    if(value && value.constructor.name === 'Object')
+    {
+      const entries = Object.entries(value)
+
+      return await Promise.all(
+        fn({value, location}),
+        ...entries.map(([key, value]) => walk(value, [...location, key]))
+      )
+    }
+
+    // We've reached a leaf node, call fn on the leaf with the location
+    await fn({value, location, isLeaf: true})
+  }
+
+  walk(root)
 }
